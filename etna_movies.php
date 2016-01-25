981a66450ecef88bddcb1b998ea7aeef7c37e89a
root <root@debian.clwd-etna.fr>
2016-01-25 14:32:36 +0100

    loic

---
+++ etna_movies.php
@@ -0,0 +1,103 @@
+<?php
+
+function connexion()
+{
+	$connect = new MongoClient();
+	$db = $connect->db_etna;
+
+	$collection = $db->students;
+}
+
+function verif_age()
+{
+	$str = readLine();
+	while (preg_match_all("/^[0-9]{1,2}$/", $str) == 0)
+	{
+		echo "Age incorrect !\n> ";
+		$str = readLine();
+	}
+	return ($str);
+}
+
+function verif_mail()
+{
+	$str = readLine();
+	while (preg_match_all("/^[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,4}$/", $str) == 0)
+	{
+		echo "Mail incorrect !\n> ";
+		$str = readLine();
+	}
+	return ($str);
+}
+
+function verif_name()
+{
+	$str = readLine();
+	while (preg_match_all("/^[ a-zA-Z-]+$/", $str) == 0)
+	{
+		echo "Nom incorrect !\n> ";
+		$str = readLine();
+	}
+        return ($str);
+}
+
+function champ_vide()
+{
+	$str = readLine();
+        while (strcmp($str, "") == 0)
+	{
+		echo "Champ obligatoire !\n> ";
+                $str = readLine();
+	}
+	return ($str);
+}
+
+function verif_phone()
+{
+	$str = readLine();
+	while (preg_match_all("/^0[1-68]([-. ]?[0-9]{2}){4}$/", $str) == 0)
+	{
+		echo "Numéro invalide !\n> ";
+		$str = readLine();
+	}
+	return ($str);
+}	
+
+function add_student($argv)
+{
+	if (preg_match_all("/[a-z]{6}_[a-z0-9]/", $argv[2], $array))
+	{
+		echo "Nom ?\n> ";
+		$name = verif_name();
+		echo "Age ?\n> ";
+		$age = verif_age();
+		echo "Email ?\n> ";
+		$mail = verif_mail();
+		echo "Numéro de téléphone ?\n> ";
+		$phone = verif_phone();
+		echo "Utilisateur enregistré \n";
+	}
+	else
+		echo "Login incorrect !\n";
+}
+
+function verif($argv)
+{
+	if	(isset($argv[3]))
+	   echo "Trop d'arguments !\n";
+	else if	    (!isset($argv[2]))
+	   echo "Pas assez d'arguments !\n";
+	else if (isset($argv[1]) && isset($argv[2]))
+	{
+		$ptr = $argv[1];
+		if (function_exists($ptr) == true)
+		   $ptr($argv);
+		else
+		   echo "Argument incorrect\n";
+	
+	}
+
+}
+
+verif($argv);
+?>
