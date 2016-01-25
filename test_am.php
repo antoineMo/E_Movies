3afa4082d05c95a6f6fd33394206e66c1d2772a6
antoineMo <moriss_a@etna-alternance.net>
2016-01-25 14:30:42 +0100

    up

--- test_am.php
+++ test_am.php
@@ -5,16 +5,20 @@ $db = $connect->db_am;
 
 $collection = $db->students;
 
-$log = "moriss_a";
+$log = "moriss_c";
 $document = array( "login" => $log, "name" => "antoine", "age" => 20, "email" => "mail@mail.net", "phone" => "0101010101");
 $collection->insert($document);
+
+$newdata = array('$set' => array("age" => 30));
+$collection->update(array("login" => "moriss_c"), $newdata);
+
 
 $cursor = $collection->find();
 
 // traverse les résultats
 foreach ($cursor as $document) {
    echo $document["login"] . "\n";
-   }
+}
 
 
 ?>
