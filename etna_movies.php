db396aac0342c7c1990cd66797b44ebb21e35773
loic perrin <perrin_l@debian.clwd-etna.fr>
2016-01-26 13:06:28 +0100

    k

--- etna_movies.php
+++ etna_movies.php
@@ -32,61 +32,6 @@ function movies_storing($argv)
 	echo $i . " films ajoutés !\n";
 }
 
-function verif_age()
-{
-	$str = readLine();
-	while (preg_match_all("/^[0-9]{1,2}$/", $str) == 0)
-	{
-		echo "Age incorrect !\n> ";
-		$str = readLine();
-	}
-	return ($str);
-}
-
-function verif_mail()
-{
-	$str = readLine();
-	while (preg_match_all("/^[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,4}$/", $str) == 0)
-	{
-		echo "Mail incorrect !\n> ";
-		$str = readLine();
-	}
-	return ($str);
-}
-
-function verif_name()
-{
-	$str = readLine();
-	while (preg_match_all("/^[ a-zA-Z-]+$/", $str) == 0)
-	{
-		echo "Nom incorrect !\n> ";
-		$str = readLine();
-	}
-        return ($str);
-}
-
-function champ_vide()
-{
-	$str = readLine();
-        while (strcmp($str, "") == 0)
-	{
-		echo "Champ obligatoire !\n> ";
-                $str = readLine();
-	}
-	return ($str);
-}
-
-function verif_phone()
-{
-	$str = readLine();
-	while (preg_match_all("/^0[1-68]([-. ]?[0-9]{2}){4}$/", $str) == 0)
-	{
-		echo "Numéro invalide !\n> ";
-		$str = readLine();
-	}
-	return ($str);
-}	
-
 require_once('verif_movie.php');
 
 
