6a08d195de1103726b326b9a6a9da4c58c5c2668
loic perrin <perrin_l@debian.clwd-etna.fr>
2016-01-26 13:01:13 +0100

    a

--- etna_movies.php
+++ etna_movies.php
@@ -2,9 +2,37 @@
 
 	$connect = new MongoClient();
 	$db = $connect->db_etna;
-
 	$collection = $db->students;
 
+
+function movies_storing($argv)
+{
+	$connect = new MongoClient();
+        $db = $connect->db_etna;
+	$collection = $db->movies;
+        $file = "movies.csv";
+	if (is_readable($file) == true)
+	{
+		$handle = fopen($file, "r");
+		while (($array = fgetcsv($handle, 1000, ",")) !== false)
+		      $tab[] = $array;
+        }
+	for ($i = 1; isset($tab[$i]); $i++)
+	{
+		$int = rand(0, 5);
+		$document = array( "imdb_code" => $tab[$i][1],
+			    	  "title" => $tab[$i][5],
+				  "year" => $tab[$i][11],
+				  "genres" => $tab[$i][15],
+				  "directors" => $tab[$i][7],
+				  "rate" => $tab[$i][9],
+				  "link" => $tab[$i][15],
+				  "stock" => $int
+				  );
+		$collection->insert($document);
+	}
+	echo $i . " films ajoutés !\n";
+}
 
 function verif_age()
 {
@@ -63,6 +91,11 @@ function verif_phone()
 
 function del_student($argv)
 {
+	if (!isset($argv[2]))
+	{
+		echo "Pas assez d'argument\n";
+		return (0);
+	}
 	$connect = new MongoClient();
 	$db = $connect->db_etna;
         $collection = $db->students;
@@ -71,7 +104,6 @@ function del_student($argv)
 	{
 		echo "Are you sure ? oui/non\n> ";
 		$str = readLine();
-		
 		if (strcmp($str, "oui") == 0 || strcmp($str, "yes") == 0)
 		{
 			$collection->remove(array('login' => $argv[2]));
@@ -87,11 +119,15 @@ function del_student($argv)
 	}
 	else
 	 echo "Cet utilisateur n'est pas enregistré !\n";
-
 }
 
 function add_student($argv)
 {
+	if (!isset($argv[2]))
+	{
+		echo "Pas assez d'argument\n";
+		return (0);
+	}
 	if (preg_match_all("/[a-z]{1,6}_[a-z0-9]/", $argv[2], $array))
 	{
 		echo "Nom ?\n> ";
@@ -118,11 +154,13 @@ function add_student($argv)
 
 function show_student($argv)
 {
+	$connect = new MongoClient();
+        $db = $connect->db_etna;
+	        $collection = $db->students;
 	if (preg_match_all("/[a-z]{6}_[a-z0-9]/", $argv[2], $array))
 	{
 
 		$cursor = $collection->findOne(array("login" => $argv[2]));
-
 		if (isset($cursor))
 		{
 			echo "login : " . $cursor["login"] . "\n";
@@ -141,9 +179,7 @@ function verif($argv)
 {
 	if	(isset($argv[3]))
 	   echo "Trop d'arguments !\n";
-	else if	    (!isset($argv[2]))
-	   echo "Pas assez d'arguments !\n";
-	else if (isset($argv[1]) && isset($argv[2]))
+	else if (isset($argv[1]))
 	{
 		$ptr = $argv[1];
 		if (function_exists($ptr) == true)
@@ -152,6 +188,9 @@ function verif($argv)
 		   echo "Argument incorrect\n";
 	
 	}
+	else
+	echo "Pas assez d'arguemtns !\n";
+	
 
 }
 
